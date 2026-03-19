11a19de3de407a7f2f86f93b0d36f2bb4c988129
daitore <daitore369@gmail.com>
2026-03-19 17:15:11 +0900

    third commit

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 //親から渡された「封筒」を開けています
 const Todo = ({todo,toggleTodo}) => {
-//todo: 表示する中身 toggleTodo: 親から預かった「チェックを切り替えるためのリモコン（関数）
+// todo: 表示する中身 toggleTodo: 親から預かった「チェックを切り替えるためのリモコン（関数）
     const  handleTodoClic = () => {
-        toggleTodo(todo.id)//toggleTodoのボタンを押して、親に**「このID（todo.id）のチェックを切り替えて！」**とメッセージを送ります。
+        toggleTodo(todo.id){/* toggleTodoのボタンを押して、親に「このID（todo.id）のチェックを切り替えて！」とメッセージを送ります。 */}
     };
 
     return  (
